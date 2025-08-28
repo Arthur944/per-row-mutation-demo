2b14711107a884a772f1dbec0d603353c7296314
Arthur944 <23a23r23t23@gmail.com>
2025-08-28 05:09:56 +0200

    initial commit

---
+++ src/components/ui/data-table.tsx
@@ -0,0 +1,83 @@
+import { flexRender, Table as ReactTable, Row } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export function DataTable<T>({
+  table,
+  rowClassName,
+  onRowClick,
+}: {
+  table: ReactTable<T>;
+  rowClassName?: string;
+  onRowClick?: (row: Row<T>) => void;
+}) {
+  return (
+    <div className="w-full">
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      style={{ width: header.getSize() + "px" }}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className={rowClassName}
+                  onClick={() => onRowClick?.(row)}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell
+                      key={cell.id}
+                      style={{ width: cell.column.getSize() + "px" }}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow className={rowClassName}>
+                <TableCell
+                  colSpan={table.getVisibleFlatColumns().length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,197 @@
+import { createFileRoute } from "@tanstack/react-router";
+import logo from "../logo.svg";
+import { DataTable } from "@/components/ui/data-table";
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { CircleAlert, CloudDownload, Loader2 } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  useMutation,
+  useMutationState,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+
+export const Route = createFileRoute("/")({
+  component: App,
+});
+
+function useKiosks() {
+  return useQuery({
+    queryKey: ["kiosks"],
+    queryFn: async () => {
+      return data;
+    },
+  });
+}
+
+function useUpdateMutation() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: ["update"],
+    mutationFn: async (args: { id: number }) => {
+      const duration = Math.random() * 5000;
+      await new Promise((resolve) => setTimeout(resolve, duration));
+
+      const kiosk = data.find((kiosk) => kiosk.id === args.id)!;
+      if (Math.random() > 0.5) {
+        throw new Error("Something went wrong");
+      }
+
+      const [, , prev] = kiosk.version.split(".");
+      kiosk.version = `1.0.${Number(prev) + 1}`;
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["kiosks"] });
+    },
+  });
+}
+
+function App() {
+  const updateMutation = useUpdateMutation();
+  const kiosks = useKiosks();
+  const table = useReactTable({
+    data: kiosks.data ?? [],
+    getRowId: (row) => row.id.toString(),
+    getCoreRowModel: getCoreRowModel(),
+    columns,
+  });
+
+  const selectedIds = Object.keys(table.getState().rowSelection);
+
+  return (
+    <div className="p-4">
+      <div
+        className={cn(
+          "py-2",
+          selectedIds.length === 0 && "opacity-0 pointer-events-none",
+        )}
+      >
+        <Button
+          onClick={() => {
+            selectedIds.forEach((id) =>
+              updateMutation.mutate({ id: Number(id) }),
+            );
+            table.resetRowSelection();
+          }}
+        >
+          Update selected
+        </Button>
+      </div>
+      <DataTable table={table} onRowClick={(row) => row.toggleSelected()} />
+    </div>
+  );
+}
+
+interface Data {
+  id: number;
+  name: string;
+  version: string;
+}
+
+const columnHelper = createColumnHelper<Data>();
+
+const data: Data[] = [
+  { id: 1, name: "Kiosk 1", version: "1.0.0" },
+  { id: 2, name: "Kiosk 2", version: "1.0.0" },
+  { id: 3, name: "Kiosk 3", version: "1.0.0" },
+  { id: 4, name: "Kiosk 4", version: "1.0.0" },
+  { id: 5, name: "Kiosk 5", version: "1.0.0" },
+  { id: 6, name: "Kiosk 6", version: "1.0.0" },
+  { id: 7, name: "Kiosk 7", version: "1.0.0" },
+  { id: 8, name: "Kiosk 8", version: "1.0.0" },
+  { id: 9, name: "Kiosk 9", version: "1.0.0" },
+  { id: 10, name: "Kiosk 10", version: "1.0.0" },
+  { id: 11, name: "Kiosk 11", version: "1.0.0" },
+  { id: 12, name: "Kiosk 12", version: "1.0.0" },
+  { id: 13, name: "Kiosk 13", version: "1.0.0" },
+  { id: 14, name: "Kiosk 14", version: "1.0.0" },
+];
+
+const columns = [
+  columnHelper.display({
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+      />
+    ),
+    maxSize: 50,
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={row.getToggleSelectedHandler()}
+      />
+    ),
+  }),
+  columnHelper.accessor("name", {
+    header: "Name",
+  }),
+  columnHelper.accessor("version", {
+    header: "version",
+    cell: ({ getValue }) => <Badge>{getValue()}</Badge>,
+  }),
+  columnHelper.display({
+    id: "actions",
+    header: "",
+    cell: ({ row }) => {
+      const updateMutation = useUpdateMutation();
+      const mutations = useMutationState({
+        filters: {
+          mutationKey: ["update"],
+          predicate: (mutation) =>
+            (mutation.state.variables as { id: number })?.id ===
+            row.original.id,
+        },
+      });
+      const latest = mutations[mutations.length - 1];
+      const pending = latest?.status === "pending";
+      const error = latest?.error;
+
+      return (
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            if (pending) {
+              return;
+            }
+            updateMutation.mutate({ id: row.original.id });
+          }}
+        >
+          {pending ? (
+            <Loader2 className="animate-spin" />
+          ) : error ? (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <CircleAlert className="text-red-500" />
+              </TooltipTrigger>
+              <TooltipContent>{error.message}</TooltipContent>
+            </Tooltip>
+          ) : (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <CloudDownload />
+              </TooltipTrigger>
+              <TooltipContent>Update</TooltipContent>
+            </Tooltip>
+          )}
+        </div>
+      );
+    },
+  }),
+];
